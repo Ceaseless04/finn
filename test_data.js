89fee33bb967c25255a49e6f149223f674ded085
Ceaseless04 <kristian120304@gmail.com>
2023-11-05 00:41:00 -0400

    working on catch error message

--- test_data.js
+++ test_data.js
@@ -1,2 +1,10 @@
 import { restClient } from '@polygon.io/client-js';
+import 'dotenv/config';
+
 const rest = restClient(process.env.POLY_KEY);
+
+rest.stocks.aggregates("AAPL", 1, "day", "2023-01-01", "2023-04-14").then((data) => {
+	console.log(data);
+}).catch(e => {
+	console.error('An error happened:', e);
+});
