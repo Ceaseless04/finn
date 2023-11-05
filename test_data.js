410e6a01745c77e736c1995601d8cadc789759ad
Ceaseless04 <kristian120304@gmail.com>
2023-11-05 13:48:16 -0500

    api finally works, calculated average of high cost for Apple stock

--- test_data.js
+++ test_data.js
@@ -3,8 +3,23 @@ import 'dotenv/config';
 
 const rest = restClient(process.env.POLY_KEY);
 
+// for aggregates
 rest.stocks.aggregates("AAPL", 1, "day", "2023-01-01", "2023-04-14").then((data) => {
-	console.log(data);
+	console.log(data.results);
+	const high_cos_map = data.results.map((high) => high.h);
+	console.log(high_cos_map);
+
+	let sum = 0.0;
+	let avg = 0.0;
+	let counter = 0;
+
+	high_cos_map.forEach((cost) => {
+		sum += cost;
+		counter++;
+		avg = sum / counter;
+	});
+
+	console.log(`The Average high cost for Apple stock price is: ${avg}`);
 }).catch(e => {
 	console.error('An error happened:', e);
 });
