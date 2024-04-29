3478d7233707710d6e8f2ea35e0197048e535ce1
Ceaseless04 <kristian120304@gmail.com>
2024-04-29 00:18:34 -0400

    WIP, backend functionality and learning how to use polygon API

---
+++ server/src/apis/polygon.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	polygon "github.com/polygon-io/client-go/rest"
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+// function to get exponential moving average
+func getEMA() {
+	
+	// new client using the polygon api
+	client := polygon.New(os.Getenv("POLYGON_API"));
+
+	// set params
+	params := &models.GetLastTradeParams{
+		Ticker: "AAPL",
+	};
+
+	// make request
+	res, err := client.GetLastTrade(context.Background(), params);
+	if err != nil {
+		fmt.Println(err);
+	}
+
+	// print result
+	fmt.Println(res);
+
+	fmt.Println(client);
+}
